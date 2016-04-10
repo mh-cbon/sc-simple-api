53944d2888eda61adaf73cdaffbe8be00ee673f8
mh-cbon <mh-cbon@users.noreply.github.com>
2016-04-10 12:20:15 +0200

    add functional tests

--- run-tests.bat
+++ run-tests.bat
@@ -6,4 +6,5 @@ cd C:\vagrant\
 call C:\nodejsx64\npm.cmd i
 :: run mocha
 call C:\Users\vagrant\AppData\Roaming\npm\mocha.cmd test\index.js
+call C:\Users\vagrant\AppData\Roaming\npm\mocha.cmd test\funct.js
 call C:\Users\vagrant\AppData\Roaming\npm\mocha.cmd test\sp-scquery.js
